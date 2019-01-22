0ec15057d6b0c3b0ea99824896a60ce82fd953bb
amy null <null@amy.gg>
2019-01-22 10:56:55 -0500

    Start on request proxying

---
+++ lib/singyeong/proxy.ex
@@ -0,0 +1,8 @@
+defmodule Singyeong.Proxy do
+  @moduledoc """
+  Singyeong is capable of proxying HTTP requests between services, while still
+  retaining the ability to route requests by metadata.
+
+  TODO: Determine how to reasonably proxy requests between hosts without requiring a mess of request formatting ;_;
+  """
+end
