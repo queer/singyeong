916b1e71fd95174fd0659797e34e39182dbda279
amy null <null@amy.gg>
2019-01-22 10:40:36 -0500

    Make service discovery require auth if auth is configured

--- lib/singyeong_web/router.ex
+++ lib/singyeong_web/router.ex
@@ -3,6 +3,7 @@ defmodule SingyeongWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :authenticate
   end
 
   scope "/api", SingyeongWeb do
@@ -12,6 +13,25 @@ defmodule SingyeongWeb.Router do
       scope "/discovery" do
         get "/tags", DiscoveryController, :by_tags
       end
+    end
+  end
+
+  def authenticate(conn, _) do
+    auth =
+      case get_req_header(conn, "authorization") do
+        l when is_list(l) and length(l) > 0 ->
+          hd(l)
+        _ ->
+          nil
+      end
+    configured_auth = System.get_env "AUTH"
+    if configured_auth != nil and auth != configured_auth do
+      conn
+      |> put_status(401)
+      |> json(%{"status" => "error", "error" => "not authorized"})
+      |> halt()
+    else
+      conn
     end
   end
 end
--- test/singyeong_web/controllers/discovery_controller_test.exs
+++ test/singyeong_web/controllers/discovery_controller_test.exs
@@ -3,6 +3,12 @@ defmodule SingyeongWeb.DiscoveryControllerTest do
   alias SingyeongWeb.Router.Helpers, as: Routes
   alias Singyeong.MnesiaStore
 
+  setup do
+    on_exit fn ->
+      System.delete_env "AUTH"
+    end
+  end
+
   test "Can discover services via querystring tags", %{conn: conn} do
     # ugly boring gross setup stuff qwq
     MnesiaStore.initialize()
@@ -36,6 +42,42 @@ defmodule SingyeongWeb.DiscoveryControllerTest do
     one_tag_app_ids = one_tag_res["result"]
     assert ["test-app-2", "test-app-3"] == one_tag_app_ids
 
+    # ugly boring gross cleanup stuff qwq
     MnesiaStore.shutdown()
+  end
+
+  test "Can discover services with authentication", %{conn: conn} do
+    # ugly boring gross setup stuff qwq
+    MnesiaStore.initialize()
+    one_tag = ["webscale"]
+    two_tags = ["test", "test-tag"]
+    MnesiaStore.add_client "test-app-1", "client-1"
+    MnesiaStore.add_client "test-app-2", "client-1"
+    MnesiaStore.set_tags "test-app-1", "client-1", one_tag
+    MnesiaStore.set_tags "test-app-2", "client-1", two_tags
+
+    # Interesting test stuff begins here
+    System.put_env "AUTH", "1234"
+
+    res1 =
+      build_conn()
+      |> put_req_header("authorization", "12345678")
+      |> get(Routes.discovery_path(conn, :by_tags, q: "[%22webscale%22]"))
+      |> json_response(401)
+    assert "error" == res1["status"]
+
+    res2 =
+      build_conn()
+      |> put_req_header("authorization", "1234")
+      |> get(Routes.discovery_path(conn, :by_tags, q: "[%22webscale%22]"))
+      |> json_response(200)
+    assert "ok" == res2["status"]
+
+    apps = res2["result"]
+    assert ["test-app-1"] == apps
+
+    # ugly boring gross cleanup stuff qwq
+    MnesiaStore.shutdown()
+    # Env is automatically cleaned up (see setup above)
   end
 end
