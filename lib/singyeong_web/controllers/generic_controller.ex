bef1a97a19775805ff6a1939e311c8ac5a1533ff
amy null <null@amy.gg>
2019-02-20 22:26:59 -0500

    Add catch-all 404 routes

---
+++ lib/singyeong_web/controllers/generic_controller.ex
@@ -0,0 +1,16 @@
+defmodule SingyeongWeb.GenericController do
+  use SingyeongWeb, :controller
+
+  def not_found(conn, _params) do
+    conn
+    |> put_status(404)
+    |> json(%{"code" => 404, "error" => "not found"})
+  end
+
+  def versions(conn, _params) do
+    conn
+    |> json(%{
+      "singyeong" => Singyeong.version()
+    })
+  end
+end
