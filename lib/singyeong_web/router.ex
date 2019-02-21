2d64bbd220df54708a2005887a88ddca8ff2471a
amy null <null@amy.gg>
2019-02-20 22:21:40 -0500

    Add catch-all 404 routes

--- lib/singyeong_web/router.ex
+++ lib/singyeong_web/router.ex
@@ -4,6 +4,9 @@ defmodule SingyeongWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :authenticated_routes do
     plug :authenticate
   end
 
@@ -11,11 +14,29 @@ defmodule SingyeongWeb.Router do
     pipe_through :api
 
     scope "/v1" do
+      pipe_through :authenticated_routes
       scope "/discovery" do
         get "/tags", DiscoveryController, :by_tags
       end
       post "/proxy", ProxyController, :proxy
     end
+
+    get "/version", GenericController, :versions
+  end
+
+  scope "/", SingyeongWeb do
+    # lol
+    # catch all missing routes and 404 them nicely instead of Phoenix's
+    # generated error page
+    get     "/*path", GenericController, :not_found
+    post    "/*path", GenericController, :not_found
+    put     "/*path", GenericController, :not_found
+    patch   "/*path", GenericController, :not_found
+    delete  "/*path", GenericController, :not_found
+    head    "/*path", GenericController, :not_found
+    connect "/*path", GenericController, :not_found
+    options "/*path", GenericController, :not_found
+    trace   "/*path", GenericController, :not_found
   end
 
   def authenticate(conn, _) do
