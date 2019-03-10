04f31fc2ac6ae9c8e24f3619e6aafbc0be00ab58
amy null <null@amy.gg>
2019-03-10 17:39:14 -0400

    Fix phoenix not using Jason instead of Poison

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,7 @@ config :logger, :console,
 
 config :phoenix, :format_encoders,
   json: Jason
+config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
