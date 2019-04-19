7f80b49bb0044dde7df942537bb29d1b6dcc05c1
amy null <null@amy.gg>
2019-04-19 17:27:51 -0400

    Fix failing tests ;;

--- test/singyeong/gateway_test.exs
+++ test/singyeong/gateway_test.exs
@@ -17,8 +17,6 @@ defmodule Singyeong.GatewayTest do
     client_id = "client-1"
     app_id = "test-app-1"
 
-    # IO.inspect socket, pretty: true
-
     %GatewayResponse{
       response: response,
       assigns: assigns
@@ -30,21 +28,45 @@ defmodule Singyeong.GatewayTest do
           "application_id" => app_id,
           "reconnect" => false,
           "auth" => nil,
-          "tags" => ["test", "webscale"]
+          "tags" => ["test", "webscale"],
         },
-        t: :os.system_time(:millisecond)
+        t: :os.system_time(:millisecond),
       }
 
-    assert %{client_id: client_id, app_id: app_id, restricted: false} == assigns
+    assert %{client_id: client_id, app_id: app_id, restricted: false, etf: false} == assigns
 
     # Destructure it
     {:text, response} = response
-    # Decode it
-    response = Jason.decode! response
+    # Payloads are only encoded at the moment of sending, so decoding here
+    # isn't necessary
+    # response = Jason.decode! response
     # Actually test it
     assert is_map response["d"]
     d = response["d"]
     assert client_id == d["client_id"]
     refute d["restricted"]
+
+    client_id = "client-2"
+    app_id = "test-app-2"
+
+    # Test actually setting ETF mode
+    %GatewayResponse{
+      response: _response,
+      assigns: assigns
+    } =
+      Gateway.handle_identify socket, %{
+        op: Gateway.opcodes_name()[:identify],
+        d: %{
+          "client_id" => client_id,
+          "application_id" => app_id,
+          "reconnect" => false,
+          "auth" => nil,
+          "tags" => ["test", "webscale"],
+          "etf" => true,
+        },
+        t: :os.system_time(:millisecond),
+      }
+
+    assert %{client_id: client_id, app_id: app_id, restricted: false, etf: true} == assigns
   end
 end
