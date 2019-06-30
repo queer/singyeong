93eefb75ed0b09a082bed518f1bc31879446116f
amy null <null@amy.gg>
2019-06-29 20:54:55 -0400

    Add real tests for encoding

---
+++ test/singyeong/encoding_test.exs
@@ -0,0 +1,35 @@
+defmodule Singyeong.EncodingTest do
+  use ExUnit.Case
+  alias Singyeong.Gateway
+  alias Singyeong.Gateway.Payload
+
+  test "that JSON payload encoding works" do
+    {:text, payload} = Payload.create_payload :dispatch, %{"test" => "test"}
+    {:text, encoded} = Gateway.encode_real "json", payload
+    decoded = Jason.decode! encoded
+    assert payload.ts == decoded["ts"]
+    assert payload.t == decoded["t"]
+    assert payload.op == decoded["op"]
+    assert payload.d["test"] == decoded["d"]["test"]
+  end
+
+  test "that ETF payload encoding works" do
+    {:text, payload} = Payload.create_payload :dispatch, %{"test" => "test"}
+    {:binary, encoded} = Gateway.encode_real "etf", payload
+    decoded = :erlang.binary_to_term encoded
+    assert payload.ts == decoded[:ts]
+    assert payload.t == decoded[:t]
+    assert payload.op == decoded[:op]
+    assert payload.d["test"] == decoded[:d]["test"]
+  end
+
+  test "that msgpack payload encoding works" do
+    {:text, payload} = Payload.create_payload :dispatch, %{"test" => "test"}
+    {:binary, encoded} = Gateway.encode_real "msgpack", payload
+    decoded = Msgpax.unpack! encoded
+    assert payload.ts == decoded["ts"]
+    assert payload.t == decoded["t"]
+    assert payload.op == decoded["op"]
+    assert payload.d["test"] == decoded["d"]["test"]
+  end
+end
