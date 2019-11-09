e8f938845fbc1db604c4c93ba52f288e3ffbe0a0
amy null <null@amy.gg>
2019-11-09 18:42:04 -0500

    Add utils test to make codecov shut up

---
+++ test/singyeong/utils_test.exs
@@ -0,0 +1,14 @@
+defmodule Singyeong.UtilsTest do
+  use ExUnit.Case
+  alias Singyeong.Utils
+
+  test "that list concat works" do
+    assert [1, 2] == Utils.fast_list_concat([1], [2])
+    refute [2, 1] == Utils.fast_list_concat([1], [2])
+  end
+
+  test "that checking for modules existing works" do
+    assert Utils.module_compiled?(String)
+    refute Utils.module_compiled?(:this_module_does_not_exist)
+  end
+end
