7bb212046c95d80c346e450255ce820d3bfbdddc
amy null <null@amy.gg>
2019-11-11 00:56:18 -0500

    Start on plugin manager testing

---
+++ test/singyeong/plugin_manager_test.exs
@@ -0,0 +1,9 @@
+defmodule Singyeong.PluginManagerTest do
+  use ExUnit.Case
+  alias Singyeong.{PluginManager, Utils}
+
+  test "that plugin loading works" do
+    PluginManager.load_plugin_from_zip "priv/test/plugin/singyeong_plugin_test.zip"
+    assert Utils.module_compiled? SingyeongPluginTest
+  end
+end
