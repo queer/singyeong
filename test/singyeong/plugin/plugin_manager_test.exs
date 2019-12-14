0ec62a5fdc519f628fd3455245b67d29a01569af
amy null <null@amy.gg>
2019-12-14 17:26:40 -0500

    Load plugin as part of plugin dispatch test, assert plugin array

--- test/singyeong/plugin/plugin_manager_test.exs
+++ test/singyeong/plugin/plugin_manager_test.exs
@@ -3,6 +3,7 @@ defmodule Singyeong.PluginManagerTest do
   alias Singyeong.Utils
 
   test "that plugin loading works" do
+    Singyeong.PluginManager.load_plugin_from_zip "priv/test/plugin/singyeong_plugin_test.zip"
     assert Utils.module_loaded? SingyeongPluginTest
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,1 @@
-Singyeong.PluginManager.load_plugin_from_zip "priv/test/plugin/singyeong_plugin_test.zip"
 ExUnit.start()
