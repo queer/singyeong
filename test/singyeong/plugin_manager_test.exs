0bffc5941117ba2af676085ffc4caa772e239789
amy null <null@amy.gg>
2019-12-14 17:07:55 -0500

    Load plugins in test helper

--- test/singyeong/plugin_manager_test.exs
+++ test/singyeong/plugin_manager_test.exs
@@ -1,9 +1,8 @@
 defmodule Singyeong.PluginManagerTest do
   use ExUnit.Case
-  alias Singyeong.{PluginManager, Utils}
+  alias Singyeong.Utils
 
   test "that plugin loading works" do
-    PluginManager.load_plugin_from_zip "priv/test/plugin/singyeong_plugin_test.zip"
     assert Utils.module_loaded? SingyeongPluginTest
   end
 end
