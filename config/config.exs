afb751d7564c5e31510ea8fd2327258323023370
amy null <null@amy.gg>
2019-12-14 14:03:01 -0500

    use Mix.Config for compatibility (for now)

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
-import Config
+use Mix.Config
 
 # Configures the endpoint
 config :singyeong, SingyeongWeb.Endpoint,
