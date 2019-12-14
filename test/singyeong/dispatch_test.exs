90f19b16f554d7b146f2d45f310f0345fd300037
amy null <null@amy.gg>
2019-12-14 14:46:56 -0500

    Close #72

--- test/singyeong/dispatch_test.exs
+++ test/singyeong/dispatch_test.exs
@@ -90,6 +90,75 @@ defmodule Singyeong.DispatchTest do
     assert 10 > abs(msg.ts - expected.ts)
   end
 
+  test "that SEND with nil ops in the query works as expected", %{socket: socket} do
+    # IDENTIFY with the gateway so that we have everything we need set up
+    # This is tested in another location
+    Gateway.handle_identify socket, %{
+      op: Gateway.opcodes_name()[:identify],
+      d: %{
+        "client_id" => @client_id,
+        "application_id" => @app_id,
+        "reconnect" => false,
+        "auth" => nil,
+        "tags" => ["test", "webscale"]
+      },
+      ts: :os.system_time(:millisecond),
+      t: nil,
+    }
+
+    target = %{
+      "application" => @app_id,
+      "optional" => true,
+      "ops" => nil
+    }
+    payload = %{}
+    nonce = "1"
+    # Actually do and test the dispatch
+    dispatch =
+      %Payload{
+        t: "SEND",
+        d: %{
+          "target" => target,
+          "payload" => payload,
+          "nonce" => nonce,
+        }
+      }
+
+    {:ok, frames} = Dispatch.handle_dispatch socket, dispatch
+    now = :os.system_time :millisecond
+    op = Gateway.opcodes_name()[:dispatch]
+    assert [] == frames
+    expected =
+      %Payload{
+        d: %{
+          "payload" => payload,
+          "nonce" => nonce
+        },
+        op: op,
+        ts: now,
+        t: "SEND",
+      }
+
+    Process.sleep 100
+    # assert_receive / assert_received exist, but we have the issue that
+    # waiting for a message to be received may actually take longer than 1ms -
+    # especially if we "send" the initial message near the end of a millisecond
+    # - meaning that we could have timestamp variance between the expected
+    # ts and the ts that the gateway sends back. We can get around this by
+    # pulling the message from the process' mailbox ourselves, and do the
+    # "pattern matching" manually with == instead of doing a real pattern
+    # match.
+    {:messages, msgs} = :erlang.process_info self(), :messages
+    {opcode, msg} = hd msgs
+    assert :text == opcode
+    assert expected.d == msg.d
+    assert expected.op == msg.op
+    assert expected.t == msg.t
+    # Really this should be within ~1ms or so, but there's a host of possible
+    # things that could make it not work out.
+    assert 10 > abs(msg.ts - expected.ts)
+  end
+
   test "that metadata updates work as expected", %{socket: socket} do
     @client_id = "client-1"
     @app_id = "test-app-1"
