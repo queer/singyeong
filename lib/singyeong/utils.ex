ab7fc8fe8961acf9382bc19e755530af077f01d3
amy null <null@amy.gg>
2020-09-06 00:46:36 -0400

    Use payloads everywhere correctly

--- lib/singyeong/utils.ex
+++ lib/singyeong/utils.ex
@@ -1,6 +1,6 @@
 defmodule Singyeong.Utils do
   @moduledoc """
-  Some utility functions that don't really belong in any one place
+  Some utility functions that don't really belong in any one place.
   """
 
   @spec fast_list_concat(list(), list()) :: list()
@@ -81,4 +81,24 @@ defmodule Singyeong.Utils do
       template_part == actual_part or String.starts_with?(template_part, ":")
     end)
   end
+
+  def stringify_keys(map, recurse? \\ false)
+
+  def stringify_keys(map, recurse?) when is_map(map) do
+    map
+    |> Enum.map(fn {k, v} ->
+      if is_binary(k) do
+        {k, stringify_keys(v)}
+      else
+        if recurse? do
+          {Atom.to_string(k), stringify_keys(v)}
+        else
+          {Atom.to_string(k), v}
+        end
+      end
+    end)
+    |> Enum.into(%{})
+  end
+
+  def stringify_keys(not_map, _), do: not_map
 end
