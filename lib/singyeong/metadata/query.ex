7c7391dec6e74ec7055ed5b3652be4cc56f0d6cf
amy null <null@amy.gg>
2020-09-15 20:33:51 -0400

    Smarter query creation (a struct!!!)

--- lib/singyeong/metadata/query.ex
+++ lib/singyeong/metadata/query.ex
@@ -8,15 +8,142 @@ defmodule Singyeong.Metadata.Query do
   advantage of the specific backend being used.
   """
 
+  use TypedStruct
   alias Singyeong.MnesiaStore, as: Store
   alias Singyeong.Utils
 
+  @boolean_op_names [
+    "$eq",
+    "$ne",
+    "$gt",
+    "$gte",
+    "$lt",
+    "$lte",
+    "$in",
+    "$nin",
+    "$contains",
+    "$ncontains",
+  ]
+  @logical_op_names [
+    "$and",
+    "$or",
+    "$nor",
+  ]
+
   @opaque query_op_result() :: {:ok, boolean()} | {:error, binary()}
+  @type key() :: binary()
+  @type value() :: term()
+  @type application_id() :: binary()
+  @type client_id() :: binary()
+  @type ops() :: [op()] | []
+  @type boolean_op_name() ::
+    :"$eq"
+    | :"$ne"
+    | :"$gt"
+    | :"$gte"
+    | :"$lt"
+    | :"$lte"
+    | :"$in"
+    | :"$nin"
+    | :"$contains"
+    | :"$ncontains"
+
+  @type logical_op_name() ::
+    :"$and"
+    | :"$or"
+    | :"$nor"
+
+  @type boolean_op() :: %{
+      required(boolean_op_name()) => value()
+    }
+
+  @type logical_op() :: %{
+    required(logical_op_name()) => maybe_improper_list(boolean_op(), logical_op())
+  }
+
+  @type op() :: %{
+    required(binary()) => boolean_op() | logical_op()
+  }
+
+  typedstruct do
+    field :application, binary(), enforce: true
+    field :restricted, boolean() | nil
+    field :key, binary() | nil
+    field :droppable, boolean() | nil
+    field :optional, boolean() | nil
+    field :ops, ops(), enforce: true
+  end
+
+  @spec json_to_query(map()) :: __MODULE__.t()
+  def json_to_query(json) do
+    %__MODULE__{
+      application: json["application"],
+      restricted: coerce_to_boolean(json["restricted"]),
+      key: coerce_to_nil_binary(json["key"]),
+      droppable: coerce_to_nil_binary(json["droppable"]),
+      optional: coerce_to_boolean(json["optional"]),
+      ops: extract_ops(json["ops"])
+    }
+  end
+
+  defp coerce_to_boolean(term) do
+    case term do
+      true -> true
+      false -> false
+      "true" -> true
+      "false" -> false
+      _ -> false
+    end
+  end
+
+  defp coerce_to_nil_binary(term) do
+    if is_binary(term), do: term, else: nil
+  end
+
+  defp extract_ops(ops) do
+    # Something something don't trust users X:
+    (ops || [])
+    |> Enum.filter(fn map ->
+      if is_map(map) do
+        key =
+          map
+          |> Map.keys
+          |> hd
+
+        op = Map.get map, key
+        recursive_check_op op
+      else
+        false
+      end
+    end)
+    |> Enum.map(&recursive_atomify_op/1)
+  end
+
+  defp recursive_check_op(map) when is_map(map) do
+    op = map |> Map.keys |> hd
+    value = Map.get map, op
+    if op in @logical_op_names and is_list(value) do
+      value |> Enum.all?(&recursive_check_op/1)
+    else
+      op in @boolean_op_names
+    end
+  end
+
+  defp recursive_atomify_op(map) when is_map(map) do
+    op = map |> Map.keys |> hd
+    value = Map.get map, op
+    if op in @logical_op_names and is_list(value) do
+      %{String.to_atom(op) => Enum.map(value, &recursive_atomify_op/1)}
+    else
+      %{String.to_atom(op) => value}
+    end
+  end
 
   @doc """
   Given a query, execute it and return a list of client IDs.
   """
-  def run_query(query, broadcast) when is_map(query) do
+  @spec run_query(__MODULE__.t(), boolean()) :: {application_id(), [client_id()] | []} | {nil, []}
+  def run_query(%__MODULE__{} = query, broadcast) do
     application = query_app_target query
     case application do
       nil ->
@@ -27,15 +154,15 @@ defmodule Singyeong.Metadata.Query do
     end
   end
 
-  defp query_app_target(query) when is_map(query) do
+  defp query_app_target(%__MODULE__{} = query) do
     cond do
-      is_binary query["application"] ->
+      is_binary query.application ->
         # Normal case, just return the application name
-        query["application"]
+        query.application
 
-      is_list query["application"] ->
+      is_list query.application ->
         # If we're passed a list, try to discover the application id
-        {:ok, matches} = Singyeong.Discovery.discover_service query["application"]
+        {:ok, matches} = Singyeong.Discovery.discover_service query.application
         if matches == [] do
           nil
         else
@@ -49,14 +176,14 @@ defmodule Singyeong.Metadata.Query do
   end
 
   defp process_query(query, application, broadcast) do
-    allow_restricted = query["restricted"]
+    allow_restricted = query.restricted
     ops =
       if allow_restricted do
         # If we allow restricted-mode clients, just run the query as-is
-        query["ops"]
+        query.ops
       else
         # Otherwise, explicitly require clients to not be restricted
-        Utils.fast_list_concat query["ops"], [%{"restricted" => %{"$eq" => false}}]
+        Utils.fast_list_concat query.ops, [%{"restricted" => %{:"$eq" => false}}]
       end
     {:ok, clients} = Store.get_clients application
     res =
@@ -66,22 +193,12 @@ defmodule Singyeong.Metadata.Query do
       |> Enum.map(fn({client, _}) -> client end)
 
     cond do
-      Enum.empty?(res) and query["optional"] == true ->
+      Enum.empty?(res) and query.optional == true ->
         # If the query is optional, and the query returned no nodes, just return
         # all nodes and let the dispatcher figure it out
         {application, clients}
 
-      not Enum.empty?(res) and query["key"] != nil ->
-        allow_restricted = query["restricted"]
-        ops =
-          if allow_restricted do
-            # If we allow restricted-mode clients, just run the query as-is
-            query["ops"]
-          else
-            # Otherwise, explicitly require clients to not be restricted
-            Utils.fast_list_concat query["ops"], [%{"restricted" => %{"$eq" => false}}]
-          end
-
+      not Enum.empty?(res) and query.key != nil ->
         {:ok, clients} = Store.get_clients application
         clients
         |> Enum.map(fn(x) -> {x, reduce_query(application, x, ops)} end)
@@ -97,15 +214,15 @@ defmodule Singyeong.Metadata.Query do
 
   defp convert_to_dispatch_form(res, application, clients, query, broadcast) do
     cond do
-      Enum.empty?(res) and query["optional"] == true ->
+      Enum.empty?(res) and query.optional == true ->
         # If the query is optional, and the query returned no nodes, just return
         # all nodes and let the dispatcher figure it out
         {application, clients}
 
-      not Enum.empty?(res) and query["key"] != nil and not broadcast ->
+      not Enum.empty?(res) and query.key != nil and not broadcast ->
         # If the query is "consistently hashed", do the best we can to
         # ensure that it ends up on the same target client each time
-        hash = :erlang.phash2 query["key"]
+        hash = :erlang.phash2 query.key
         # :erlang.phash2/1 will return a value on the range 0..2^27-1, so
         # we just modulus it and we're done
         idx = rem hash, length(res)
@@ -188,16 +305,16 @@ defmodule Singyeong.Metadata.Query do
     end)
   end
 
-  defp operator_to_function(op) when is_binary(op) do
+  defp operator_to_function(op) when is_atom(op), do: op |> Atom.to_string |> operator_to_function
+  defp operator_to_function("$" <> op) do
     # $eq -> :op_eq
     op
-    |> String.trim("$")
     |> as_op
     |> String.to_atom
   end
 
   defp as_op(s) when is_binary(s) do
-    "op_" <> s
+    "op_#{s}"
   end
 
   ## QUERY OPERATORS ##
